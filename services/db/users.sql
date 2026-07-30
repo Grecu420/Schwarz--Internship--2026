2b308a144ef93314cc25e99fa8ea5d0c2379c157
crastan <e.ravariu@yahoo.com>
2026-07-30 17:29:46 +0300

    create database container

---
+++ services/db/users.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
