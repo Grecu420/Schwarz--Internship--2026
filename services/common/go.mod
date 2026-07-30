cb722ce90d6a5cb43aef69d7ae2ed4e836935b98
crastan <e.ravariu@yahoo.com>
2026-07-30 17:31:35 +0300

    extend userbase api

---
+++ services/common/go.mod
@@ -0,0 +1,3 @@
+module services/common
+
+go 1.26.5
--- services/user-base/main/service.go
+++ services/user-base/main/service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+
 	"log"
 	"net"
 	"os"
@@ -10,6 +11,8 @@ import (
 	"strconv"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserServiceImpl struct {
@@ -22,6 +25,10 @@ func (u UserServiceImpl) Ping(context.Context, *proto.Empty) (*proto.Pong, error
 	return &proto.Pong{Message: "pong "}, nil
 }
 
+func (u UserServiceImpl) EndpointName(context.Context, *proto.EndpointNameRequest) (*proto.EndpointNameResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method EndpointName not implemented")
+}
+
 const defaultPort = 50051
 
 func getPort() (int, error) {
@@ -31,6 +38,10 @@ func getPort() (int, error) {
 	}
 
 	return strconv.Atoi(s)
+
+}
+
+func prepareDB() {
 
 }
 
@@ -46,8 +57,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	var opts []grpc.ServerOption
 
+	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	proto.RegisterUserServiceServer(grpcServer, UserServiceImpl{})
 	grpcServer.Serve(lis)
