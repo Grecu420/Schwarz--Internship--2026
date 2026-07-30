36aa92d8300132264d93cc66d806dd524ded7133
crastan <e.ravariu@yahoo.com>
2026-07-30 12:23:41 +0300

    implement service and write docker compose

--- services/user-base/main/service.go
+++ services/user-base/main/service.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"os"
 	"services/user-base/main/proto"
+	"strconv"
+
+	"google.golang.org/grpc"
 )
 
 type UserServiceImpl struct {
+	proto.UnimplementedUserServiceServer
+}
+
+// Ping implements [proto.UserServiceServer].
+func (u UserServiceImpl) Ping(context.Context, *proto.Empty) (*proto.Pong, error) {
+	fmt.Println("here")
+	return &proto.Pong{Message: "pong "}, nil
+}
+
+const defaultPort = 50051
+
+func getPort() (int, error) {
+	s, ok := os.LookupEnv("PORT")
+	if !ok {
+		return 0, fmt.Errorf("no port variable")
+	}
+
+	return strconv.Atoi(s)
+
 }
 
 func main() {
 
-	var x proto.Empty
+	var port int = defaultPort
+	p, err := getPort()
+	if err == nil {
+		port = p
+	}
+	fmt.Println("Port: ", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf("user-base-service:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
 
-	fmt.Println(x.String())
+	grpcServer := grpc.NewServer(opts...)
+	proto.RegisterUserServiceServer(grpcServer, UserServiceImpl{})
+	grpcServer.Serve(lis)
+
 }
