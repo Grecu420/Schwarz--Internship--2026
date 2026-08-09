adbac7479d6b9f9a8d239f4aa8994cc1724557ac
crastan <e.ravariu@yahoo.com>
2026-08-09 09:33:32 +0300

    added friend-request to api-rest-gateway

--- services/api-rest-gateway/main/service.go
+++ services/api-rest-gateway/main/service.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"Schwarz--Internship--2026/services/api-rest-gateway/user-base/proto"
+	frb "Schwarz--Internship--2026/services/api-rest-gateway/friend-request-base/proto"
+	ub "Schwarz--Internship--2026/services/api-rest-gateway/user-base/proto"
+
+	"Schwarz--Internship--2026/services/common"
 	"context"
 	"log"
 	"net/http"
@@ -22,7 +25,25 @@ func main() {
 	}
 
 	// Connect the HTTP gateway to the backend gRPC server running on localhost:50051
-	err := proto.RegisterUserServiceHandlerFromEndpoint(ctx, mux, "user-base-service:50051", opts)
+
+	user_base_endpoint, err := common.GetRequiredEnv("USER-BASE_ENDPOINT")
+	if err != nil {
+		log.Fatalf("Failed to register gateway: %v", err)
+
+	}
+
+	err = ub.RegisterUserServiceHandlerFromEndpoint(ctx, mux, user_base_endpoint, opts)
+	if err != nil {
+		log.Fatalf("Failed to register gateway: %v", err)
+	}
+
+	friend_request_base_endpoint, err := common.GetRequiredEnv("FRIEND-REQUEST-BASE_ENDPOINT")
+	if err != nil {
+		log.Fatalf("Failed to register gateway: %v", err)
+
+	}
+
+	err = frb.RegisterFriendRequestServiceHandlerFromEndpoint(ctx, mux, friend_request_base_endpoint, opts)
 	if err != nil {
 		log.Fatalf("Failed to register gateway: %v", err)
 	}
