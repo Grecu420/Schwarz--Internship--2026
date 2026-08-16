069efd78be6913892a609ec019d0e84d4e90abb9
crastan <e.ravariu@yahoo.com>
2026-08-16 17:28:37 +0300

    implemented ListFriendRequests method

--- services/friend-request-base/main/postgres.go
+++ services/friend-request-base/main/postgres.go
@@ -6,6 +6,9 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func InsertFriendRequestInDB(ctx context.Context, db *sql.DB, senderID string, receiverID string, status int32) (int64, error) {
@@ -37,36 +40,73 @@ func UpdateFriendRequestStatusInDB(ctx context.Context, db *sql.DB, id int64, ne
 	return res.RowsAffected()
 }
 
-func SelectFriendListInDB(ctx context.Context, db *sql.DB, offsetID int64, page_size int64, senderID string, receiverID string, status proto.RequestStatus) {
+func SelectFriendListInDB(ctx context.Context, db *sql.DB, offsetID int64, page_size int64, senderID string, receiverID string, status proto.RequestStatus) ([]*proto.FriendRequest, error) {
 
-	// build query
-	baseQuery := "SELECT id, name, status, role, created_at FROM users"
-	if len(whereClauses) > 0 {
-		baseQuery += " WHERE " + strings.Join(whereClauses, " AND ")
-	}
+	// Build query
+	baseQuery := `SELECT "id", "sender_id", "receiver_id", "status", "created_at" FROM "friend_requests"`
 
 	var whereClauses []string
 	var args []any
 	argNum := 1
 
-	// Optional Filter 1: Status
-	if req.GetStatusFilter() != "" {
+	// Optional Filter 1: senderID
+	if senderID != "" {
+		whereClauses = append(whereClauses, fmt.Sprintf("sender_id = $%d", argNum))
+		args = append(args, senderID)
+		argNum++
+	}
+
+	// Optional Filter 2: receiverID
+	if receiverID != "" {
+		whereClauses = append(whereClauses, fmt.Sprintf("receiver_id = $%d", argNum))
+		args = append(args, senderID)
+		argNum++
+	}
+
+	// Optional Filter 3: status
+	if status != proto.RequestStatus_STATUS_UNKNOWN {
 		whereClauses = append(whereClauses, fmt.Sprintf("status = $%d", argNum))
-		args = append(args, req.GetStatusFilter())
+		args = append(args, status)
 		argNum++
 	}
 
-	// Optional Filter 2: Role
-	if req.GetRoleFilter() != "" {
-		whereClauses = append(whereClauses, fmt.Sprintf("role = $%d", argNum))
-		args = append(args, req.GetRoleFilter())
-		argNum++
+	// Add filter for offset (starting ID)
+	whereClauses = append(whereClauses, fmt.Sprintf("id >= $%d", argNum))
+	args = append(args, offsetID)
+	argNum++
+
+	// Append where clauses
+	baseQuery += " WHERE " + strings.Join(whereClauses, " AND ")
+
+	// Append ordering and limit to page size
+	baseQuery += fmt.Sprintf(" ORDER BY id LIMIT $%d;", argNum)
+	args = append(args, page_size+1)
+
+	fmt.Println(baseQuery)
+
+	// Execute query
+	rows, err := db.QueryContext(ctx, baseQuery, args...)
+	if err != nil {
+		return nil, fmt.Errorf("database query: %w", err)
+	}
+	defer rows.Close()
+
+	// Scan rows
+	var friendRequests []*proto.FriendRequest
+	for rows.Next() {
+		var fr proto.FriendRequest
+		var createdAt time.Time
+
+		if err := rows.Scan(&fr.Id, &fr.SenderId, &fr.ReceiverId, &fr.Status, &createdAt); err != nil {
+			return nil, fmt.Errorf("scan friend request: %w", err)
+		}
+		fr.CreatedAt = timestamppb.New(createdAt)
+		friendRequests = append(friendRequests, &fr)
 	}
 
-	// Optional Filter 3: Search Query (ILIKE / partial match)
-	if req.GetSearchQuery() != "" {
-		whereClauses = append(whereClauses, fmt.Sprintf("name ILIKE $%d", argNum))
-		args = append(args, "%"+req.GetSearchQuery()+"%")
-		argNum++
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error reading database rows: %w", err)
 	}
+
+	return friendRequests, nil
 }
