b6b258827f9fed4d248dceac0267953e3731f0d9
crastan <e.ravariu@yahoo.com>
2026-08-18 11:26:33 +0300

    added ListFriendRequests endpoint to api rest gateway

--- services/api-rest-gateway/main/friend-request-service.go
+++ services/api-rest-gateway/main/friend-request-service.go
@@ -16,3 +16,7 @@ func (service GatewayServiceImpl) CreateFriendRequest(ctx context.Context, req *
 func (service GatewayServiceImpl) UpdateFriendRequest(ctx context.Context, req *proto.UpdateFriendRequestRequest) (*proto.UpdateFriendRequestResponse, error) {
 	return service.getFriendRequestServiceClient().UpdateFriendRequest(ctx, req)
 }
+
+func (service GatewayServiceImpl) ListFriendRequests(ctx context.Context, req *proto.ListFriendRequestsRequest) (*proto.ListFriendRequestsResponse, error) {
+	return service.getFriendRequestServiceClient().ListFriendRequests(ctx, req)
+}
