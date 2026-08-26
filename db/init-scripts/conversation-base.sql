dc053d837ad9dbe04cb455ad6c57a32dbf5f1f1a
crastan <e.ravariu@yahoo.com>
2026-08-26 17:42:42 +0300

    created table and protobuf api

---
+++ db/init-scripts/conversation-base.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS conversations (    
+    id BIGSERIAL PRIMARY KEY,    
+    user1_id BIGINT NOT NULL,    
+    user2_id BIGINT NOT NULL,    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+); 
+
+CREATE UNIQUE INDEX IF NOT EXISTS CONVERSATION_USER_ORDER_IDX ON conversations (LEAST(user1_id, user2_id), GREATEST(user1_id, user2_id));
