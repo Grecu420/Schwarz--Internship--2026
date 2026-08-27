1bdfd5a429af151388f288f483d993ecbaae4c8e
crastan <e.ravariu@yahoo.com>
2026-08-27 18:42:33 +0300

    implemented createconversation endpoint

--- services/conversation-base/main/createConversation.go
+++ services/conversation-base/main/createConversation.go
@@ -3,8 +3,28 @@ package main
 import (
 	"Schwarz--Internship--2026/services/conversation-base/main/proto"
 	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (service ConversationServiceImpl) CreateConversation(ctx context.Context, req *proto.CreateConversationRequest) (*proto.CreateConversationResponse, error) {
 
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	if req.GetUser1Id() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "user1 missing")
+	}
+	if req.GetUser2Id() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "user2 missing")
+	}
+
+	id, err := InsertConversation(ctx, service.DB, req.GetUser1Id(), req.GetUser2Id())
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed insert conversation to database: %v", err)
+	}
+	return &proto.CreateConversationResponse{Id: id}, nil
 }
--- services/conversation-base/main/postgres.go
+++ services/conversation-base/main/postgres.go
@@ -3,8 +3,23 @@ package main
 import (
 	"context"
 	"database/sql"
+
+	sq "github.com/Masterminds/squirrel"
 )
 
 func InsertConversation(ctx context.Context, db *sql.DB, user1ID int64, user2ID int64) (int64, error) {
+	ins := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).Insert("conversations").
+		Columns("user1_id", "user2_id").
+		Values(user1ID, user2ID).
+		Suffix("RETURNING id")
 
+	var id int64
+
+	err := ins.RunWith(db).QueryRowContext(ctx).Scan(&id)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
