1c70489d2bc89bf936395b66592554ba123a7147
Ivancu Andrei <119801460+IvancuAndrei@users.noreply.github.com>
2026-08-31 16:54:22 +0300

    modified sql files

--- db/init-scripts/message.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (    
-id BIGSERIAL PRIMARY KEY,    
-conversation_id BIGINT NOT NULL,    
-sender_id BIGINT NOT NULL,    
-content TEXT NOT NULL,     
-created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
