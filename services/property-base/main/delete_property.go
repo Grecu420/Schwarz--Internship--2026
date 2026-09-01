793a273c907110d0eab012d69b16c12f13d18079
crastan <e.ravariu@yahoo.com>
2026-09-01 14:27:33 +0300

    implemented update, delete endpoints; added postgis extension to db

--- services/property-base/main/delete_property.go
+++ services/property-base/main/delete_property.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"Schwarz--Internship--2026/services/property-base/main/proto"
+	"context"
+	"errors"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (service PropertyServiceImpl) DeleteProperty(ctx context.Context, req *proto.DeletePropertyRequest) (*proto.DeletePropertyResponse, error) {
+
+	if req.GetId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "missing id")
+	}
+
+	err := DeletePropertyInDB(ctx, service.DB, req.GetId())
+
+	if errors.Is(err, NoRowsDeleted) {
+		return nil, status.Error(codes.NotFound, "property not found")
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "query error: %v", err)
+	}
+	return &proto.DeletePropertyResponse{}, nil
+}
--- services/property-base/main/postgres.go
+++ services/property-base/main/postgres.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	sq "github.com/Masterminds/squirrel"
 )
@@ -13,9 +14,35 @@ func InsertPropertyInDB(ctx context.Context, db *sql.DB, name string, descriptio
 	ins := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).
 		Insert("properties").
 		Columns("user_id", "name", "description", "address", "location", "price").
-		Values(user_id, name, description, address, sq.Expr("ST_MakePoint(?, ?)::geography", lng, lat), price)
-
+		Values(user_id, name, description, address, sq.Expr("ST_MakePoint(?, ?)::geography", lng, lat), price).
+		Suffix("RETURNING id")
 	err := ins.RunWith(db).QueryRowContext(ctx).Scan(&generatedID)
 
 	return generatedID, err
+}
+
+var NoRowsDeleted = errors.New("no rows deleted")
+
+func DeletePropertyInDB(ctx context.Context, db *sql.DB, id int64) error {
+
+	del := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).
+		Delete("properties").
+		Where(sq.Eq{"id": id})
+
+	res, err := del.RunWith(db).ExecContext(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	r, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+	if r == 0 {
+		return NoRowsDeleted
+	}
+	return nil
+
 }
