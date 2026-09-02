de9653e510d7ad50318eb88310d831ebdcc3add1
crastan <e.ravariu@yahoo.com>
2026-09-02 18:12:51 +0300

    create getMessageList method

--- services/message-base/main/postgres.go
+++ services/message-base/main/postgres.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"Schwarz--Internship--2026/services/message-base/main/proto"
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	sq "github.com/Masterminds/squirrel"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func InsertMessage(ctx context.Context, db *sql.DB, conversationID int64, senderID int64, content string) (int64, time.Time, error) {
-	ins := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).Insert("messages").
+	ins := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).
+		Insert("messages").
 		Columns("conversation_id", "sender_id", "content").
 		Values(conversationID, senderID, content).
 		Suffix("RETURNING id, created_at")
@@ -23,4 +27,43 @@ func InsertMessage(ctx context.Context, db *sql.DB, conversationID int64, sender
 	}
 
 	return id, createdAt, nil
+}
+
+func SelectMessages(ctx context.Context, db *sql.DB, conversationID int64) ([]*proto.Message, error) {
+
+	sel := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).
+		Select("id", "conversation_id", "sender_id", "content", "created_at").
+		From("messages").
+		Where(sq.Eq{"conversation_id": conversationID})
+
+	rows, err := sel.RunWith(db).QueryContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("database query: %w", err)
+	}
+	defer rows.Close()
+
+	var messages []*proto.Message
+	for rows.Next() {
+		// Read each row
+		var message proto.Message
+		var createdAt time.Time
+
+		if err := rows.Scan(
+			&message.Id,
+			&message.ConversationId,
+			&message.SenderId,
+			&message.Content,
+			&createdAt,
+		); err != nil {
+			return nil, fmt.Errorf("scan friend request: %w", err)
+		}
+		message.CreatedAt = timestamppb.New(createdAt)
+		messages = append(messages, &message)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error reading database rows: %w", err)
+	}
+
+	return messages, nil
 }
