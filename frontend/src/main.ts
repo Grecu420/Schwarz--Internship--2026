f89c3e4b43608e7bd54b477272928923a05846f6
crastan <e.ravariu@yahoo.com>
2026-09-05 14:57:45 +0300

    added onyx dependencies, refactored proto generation, removed generated files from repo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createOnyx } from "sit-onyx";
+
+import "sit-onyx/style.css";
+// import "sit-onyx/global.css";
+import "@fontsource-variable/source-code-pro";
+import "@fontsource-variable/source-sans-3";
+
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const onyx = createOnyx({
+  // if you are using the Vue Router, make sure to pass it here be enable the router integration for onyx
+  router: router,
+});
+
+
 app.use(createPinia())
-app.use(router)
+app.use(onyx)
 
 app.mount('#app')
