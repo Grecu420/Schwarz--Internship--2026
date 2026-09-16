947889ae119cc70d1d88272303bf31f6bff6c463
crastan <e.ravariu@yahoo.com>
2026-09-16 10:34:46 +0300

    added property edit page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,7 @@ import MainView from '@/views/MainView.vue'
 import PropertyCreationView from '@/views/property/PropertyCreationView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import { useAuthStore } from '@/stores/auth'
+import PropertyEditView from '@/views/property/PropertyEditView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,45 +14,55 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
-      meta: { hideHeaderFooter: true }
+      meta: { hideHeaderFooter: true },
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView,
-      meta: { hideHeaderFooter: true }
+      meta: { hideHeaderFooter: true },
     },
     {
       path: '/main',
       name: 'main',
       component: MainView,
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       path: '/properties/create',
       name: 'createProperty',
       component: PropertyCreationView,
-      meta: { requiresAuth: true } // Protect this route
+      meta: { requiresAuth: true }, // Protect this route
+    },
+    {
+      path: '/properties/edit/:id',
+      name: 'editProperty',
+      component: PropertyEditView,
+      meta: { requiresAuth: true }, // Protect this route
     },
     {
       path: '/',
-      redirect: '/main'
+      redirect: '/main',
     },
     {
       path: '/profile',
       name: 'profile',
       component: ProfileView,
     },
-  ]
+  ],
 })
 
 // Navigation Guard
 router.beforeEach((to) => {
   const authStore = useAuthStore()
 
+  if (authStore.checkTokenExpiration()) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  }
+
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
     return { name: 'login', query: { redirect: to.fullPath } }
-  } 
+  }
 
   if ((to.name === 'login' || to.name === 'register') && authStore.isAuthenticated) {
     return { name: 'main' }
--- frontend/src/views/property/PropertyEditView.vue
+++ frontend/src/views/property/PropertyEditView.vue
@@ -11,32 +11,38 @@
     </div>
 
     <PropertyForm
-      category="JOIN AS A HOST"
-      title="List a New Property"
-      description="Share your unique space with travelers from around the world. We make it simple."
+      category="CURRENT LISTING"
+      title="Edit your Property Listing"
+      description="Make updates to yout pricing, description, or listing details. Keep information fresh"
       @submit="handleSuccess"
+      :initial-data="initialProperty"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { OnyxButton, useToast } from 'sit-onyx'
 import { iconChevronLeft } from '@sit-onyx/icons'
 import PropertyForm from '@/components/PropertyForm.vue'
 import {
-  CreatePropertyRequest,
-  CreatePropertyResponse,
+  GetPropertyResponse,
+  UpdatePropertyRequest,
+  UpdatePropertyResponse,
   type Property,
 } from '@/generated/proto/property-api'
 import { useAuthStore } from '@/stores/auth'
 import api from '@/utils/api'
 import { uploadImageToCloudinary } from '@/utils/cloudinary'
+import { onMounted, ref } from 'vue'
 
 const presetName = import.meta.env.VITE_CLOUDINARY_PROFILE_PRESET
 const router = useRouter()
+const route = useRoute()
 const authStore = useAuthStore()
 const toast = useToast()
+const initialProperty = ref<Property | null>(null)
+const isLoading = ref(true)
 
 const goBack = () => {
   router.push('/properties')
@@ -56,8 +62,24 @@ const uploadImage = async (data: [string, File]): Promise<[string, string]> => {
   }
 }
 
+onMounted(async () => {
+  const id = route.params.id
+
+  try {
+    const response = await api.get<GetPropertyResponse>('/api/property', { params: { id } })
+    initialProperty.value = response.data.property ?? null
+    if (initialProperty.value === null) {
+      goBack()
+    }
+  } catch (error) {
+    console.error('Failed to load property details:', error)
+  } finally {
+    isLoading.value = false
+  }
+})
+
 const handleSuccess = async (
-  createdProperty: Property,
+  updatedProperty: Property,
   imagesToUpload: Map<string, File>,
   urlsToDelete: string[],
 ) => {
@@ -66,23 +88,28 @@ const handleSuccess = async (
     const substitutionEntries = await Promise.all(Array.from(imagesToUpload, uploadImage))
     const urlSubstitution = new Map<string, string>(substitutionEntries)
 
-    const propertyRequest = CreatePropertyRequest.create({
-      userId: createdProperty.userId || authStore.user?.id,
-      name: createdProperty.name,
-      description: createdProperty.description,
-      address: createdProperty.address,
-      price: createdProperty.price,
-      location: createdProperty.location,
-      imageUrls: createdProperty.imageUrls.map((url) => urlSubstitution.get(url) ?? url),
+    updatedProperty.imageUrls.map((url) => urlSubstitution.get(url) ?? url)
+
+    const fieldMask: string[] = ['name', 'description', 'address', 'price', 'location', 'imageUrls']
+
+    const initProperty = initialProperty.value
+
+    if (initProperty === null) {
+      goBack()
+      return
+    }
+    const request = UpdatePropertyRequest.create({
+      property: updatedProperty,
+      fieldMask: fieldMask,
     })
 
-    console.log(propertyRequest)
-    const response = await api.post<CreatePropertyResponse>('/api/property', propertyRequest)
+    console.log(request)
+    const response = await api.patch<UpdatePropertyResponse>('/api/property', request)
 
     console.log(response)
     toast.show({
       headline: 'Success',
-      description: 'Property created successfully.',
+      description: 'Property updated successfully.',
       color: 'success',
     })
 
@@ -90,8 +117,8 @@ const handleSuccess = async (
   } catch (error: any) {
     console.log(error)
     toast.show({
-      headline: 'Creation Failed',
-      description: error?.message || 'Failed to submit property request.',
+      headline: 'Update Failed',
+      description: error?.message || 'Failed to submit update request.',
       color: 'danger',
     })
   }
