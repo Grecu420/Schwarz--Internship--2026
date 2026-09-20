2753f4b845cac5e7f6898964713a3fdcd1e02279
crastan <e.ravariu@yahoo.com>
2026-09-20 13:21:02 +0300

    added conversation creation

--- frontend/src/components/property/PropertyOwnerCard.vue
+++ frontend/src/components/property/PropertyOwnerCard.vue
@@ -11,17 +11,71 @@
       mode="outline"
       label="Ask a question"
       class="ask-question-btn"
+      @click="askQuestion"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import type { UserProfile } from '@/generated/proto/user-api';
-import { OnyxAvatar, OnyxButton } from 'sit-onyx'
+import {
+  CreateConversationRequest,
+  CreateConversationResponse,
+} from '@/generated/proto/conversation-api'
+import type { UserProfile } from '@/generated/proto/user-api'
+import { useAuthStore } from '@/stores/auth'
+import api from '@/utils/api'
+import { Axios, AxiosError } from 'axios'
+import { OnyxAvatar, OnyxButton, useToast } from 'sit-onyx'
+import { useRouter } from 'vue-router'
 
-defineProps<{
+const props = defineProps<{
   owner: UserProfile
 }>()
+
+const router = useRouter()
+const authStore = useAuthStore()
+const toast = useToast()
+
+const askQuestion = async () => {
+  // create a conversation
+
+  let conversationExists = false
+
+  try {
+    const userID = authStore.user?.id
+    const ownerID = props.owner.id
+
+    const request = CreateConversationRequest.create({
+      user1Id: userID,
+      user2Id: ownerID,
+    })
+
+    const response = await api.post<CreateConversationResponse>('/api/conversations', request)
+    conversationExists = true
+  } catch (error: any) {
+    console.log(error)
+    if (error instanceof AxiosError) {
+      console.log(error)
+    }
+
+    if (error.status === 409) {
+      // conversation exists
+      conversationExists = true
+    } else {
+
+      toast.show({
+      headline: 'Conversation Error',
+      description: error.message || 'Failed to create conversation.',
+      color: 'danger',
+    })
+    }
+
+    
+  }
+  if (conversationExists) {
+    router.push(`/conversations`)
+  }
+}
 </script>
 
 <style scoped>
@@ -45,7 +99,6 @@ defineProps<{
   font-weight: 700;
   font-size: 0.95rem;
 }
-
 
 :deep(.ask-question-btn) {
   border-radius: 9999px !important;
